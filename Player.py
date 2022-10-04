358322b783141fb10eda650ae4258e99a170ae81
ZUO H.L <60392737+daniel620@users.noreply.github.com>
2022-10-05 00:19:32 +0800

    Update web and app

--- Player.py
+++ Player.py
@@ -3,6 +3,7 @@ from scipy import stats
 
 class player():
     def __init__(self, dices=None):
+        self.dice_num_per_player = 5
         self.dice_in_hand = []
         if dices is None:
             for i in range(5):
@@ -18,6 +19,8 @@ class player():
             print('error')
     def print_dice_in_hand(self):
         print(self.dice_in_hand)
+    def output_string(self,num,point,chances):
+        return '{:d} {:d}(s) Chances: {:.2%}'.format(num,point,chances)
     def technical_analysis(self,player_num,game_dice_num,game_point_chosen,is_one_out=False):
         dice_in_hand_point_count = [0,0,0,0,0,0]
         for dice in self.dice_in_hand:
@@ -31,30 +34,48 @@ class player():
                 temp_dice_in_hand_point_count[temp_dice_in_hand_point_count.index(max(temp_dice_in_hand_point_count))] = 0
                 new_point_in_hand_max_count = temp_dice_in_hand_point_count.index(max(temp_dice_in_hand_point_count))+1
                 dice_num_added = 1
-                print('{:d}个{:d}'.format((game_dice_num) + dice_num_added,new_point_in_hand_max_count))
-                print('{:.2%}'.format(round(1-stats.binom.cdf((game_dice_num-1) + dice_num_added - dice_in_hand_point_count[new_point_in_hand_max_count-1]-one_in_hand_count, (player_num-1) * dice_num_per_player, 2 / 6),4)))
+                num1 = (game_dice_num) + dice_num_added
+                point1 = new_point_in_hand_max_count
+                chances1 = round(1-stats.binom.cdf((game_dice_num-1) + dice_num_added - dice_in_hand_point_count[new_point_in_hand_max_count-1]-one_in_hand_count, (player_num-1) * self.dice_num_per_player, 2 / 6),4)
+                print('{:d}个{:d}'.format(num1,point1))
+                print('{:.2%}'.format(chances1))
                 # Choice 2: choose 1 as the point
-                print('{:d}个{:d}'.format((player_num),1))
-                print('{:.2%}'.format(round(1-stats.binom.cdf((player_num-1)  - one_in_hand_count, (player_num-1) * dice_num_per_player, 1 / 6),4)))
-
+                num2 = player_num
+                point2 = 1
+                chances2 = round(1-stats.binom.cdf((player_num-1)  - one_in_hand_count, (player_num-1) * self.dice_num_per_player, 1 / 6),4)
+                print('{:d}个{:d}'.format(num2,point2))
+                print('{:.2%}'.format(chances2))
             else:
                 # Choice 1: add dice num while not changing dice point
                 dice_num_added = 1
-                print('{:d}个{:d}'.format((game_dice_num) + dice_num_added,game_point_chosen))
-                # print('{:.2%}'.format(round(1-stats.binom.cdf((game_dice_num + dice_num_added - dice_in_hand_point_count[game_point_chosen-1]-one_in_hand_count) * 2 - 1, (player_num-1) * dice_num_per_player, 1 / 6),4)))
-                print('{:.2%}'.format(round(1-stats.binom.cdf((game_dice_num-1) + dice_num_added - dice_in_hand_point_count[game_point_chosen-1]-one_in_hand_count, (player_num-1) * dice_num_per_player, 2 / 6),4)))
+                num1 = (game_dice_num) + dice_num_added
+                point1= game_point_chosen
+                chances1 = round(1-stats.binom.cdf((game_dice_num-1) + dice_num_added - dice_in_hand_point_count[game_point_chosen-1]-one_in_hand_count, (player_num-1) * self.dice_num_per_player, 2 / 6),4)
+                print('{:d}个{:d}'.format(num1,point1))
+                # print('{:.2%}'.format(round(1-stats.binom.cdf((game_dice_num + dice_num_added - dice_in_hand_point_count[game_point_chosen-1]-one_in_hand_count) * 2 - 1, (player_num-1) * self.dice_num_per_player, 1 / 6),4)))
+                print('{:.2%}'.format(chances1))
 
                 # Choice 2: add dice num and change dice point
                 dice_num_added = 1
-                print('{:d}个{:d}'.format((game_dice_num) + dice_num_added,point_in_hand_max_count))
-                print('{:.2%}'.format(round(1-stats.binom.cdf((game_dice_num-1) + dice_num_added - dice_in_hand_point_count[point_in_hand_max_count-1]-one_in_hand_count, (player_num-1) * dice_num_per_player, 2 / 6),4)))
+                num2 = (game_dice_num) + dice_num_added
+                point2 = point_in_hand_max_count
+                chances2 = round(1-stats.binom.cdf((game_dice_num-1) + dice_num_added - dice_in_hand_point_count[point_in_hand_max_count-1]-one_in_hand_count, (player_num-1) * self.dice_num_per_player, 2 / 6),4)
+                print('{:d}个{:d}'.format(num2,point2))
+                print('{:.2%}'.format(chances2))
         else:
             # Choice 1: add dice num while not changing dice point
             dice_num_added = 1
-            print('{:d}个{:d}'.format((game_dice_num) + dice_num_added,game_point_chosen))
-            print('{:.2%}'.format(round(1-stats.binom.cdf((game_dice_num-1) + dice_num_added - dice_in_hand_point_count[game_point_chosen-1], (player_num-1) * dice_num_per_player, 1 / 6),4)))
+            num1 = (game_dice_num) + dice_num_added
+            point1= game_point_chosen
+            chances1 = round(1-stats.binom.cdf((game_dice_num-1) + dice_num_added - dice_in_hand_point_count[game_point_chosen-1], (player_num-1) * self.dice_num_per_player, 1 / 6),4)
+            print('{:d}个{:d}'.format(num1,point1))
+            print('{:.2%}'.format(chances1))
 
             # Choice 2: add dice num and change dice point
             dice_num_added = 1
-            print('{:d}个{:d}'.format((game_dice_num) + dice_num_added,point_in_hand_max_count))
-            print('{:.2%}'.format(round(1-stats.binom.cdf((game_dice_num-1) + dice_num_added - dice_in_hand_point_count[point_in_hand_max_count-1], (player_num-1) * dice_num_per_player, 1 / 6),4)))
+            num2 = (game_dice_num) + dice_num_added
+            point2 = point_in_hand_max_count
+            chances2 = round(1-stats.binom.cdf((game_dice_num-1) + dice_num_added - dice_in_hand_point_count[point_in_hand_max_count-1], (player_num-1) * self.dice_num_per_player, 1 / 6),4)
+            print('{:d}个{:d}'.format(num2,point2))
+            print('{:.2%}'.format(chances2))
+        return self.output_string(num1,point1,chances1),self.output_string(num2,point2,chances2)
--- app.py
+++ app.py
@@ -1,21 +1,33 @@
 
 from flask import Flask, request, render_template
-import Player
-import joblib
+from Player import player
+import numpy as np
+# import joblib
 app = Flask(__name__)
+
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
     if request.method == "POST":
-        rates = float(request.form.get("rates"))
-        print(rates)
-        model1 = joblib.load("regression")
-        r1 = model1.predict([[rates]])
-        model2 = joblib.load("tree")
-        r2 = model2.predict([[rates]])
-        return(render_template("index.html", result1 = r1, result2=r2))
+        dice_in_hand = str(request.form.get("dice_in_hand"))
+        last_input = dice_in_hand 
+        try:
+            dice_in_hand = np.array(dice_in_hand.replace(' ','').split(','),dtype=int).tolist()
+            if len(dice_in_hand) != 5:
+                return render_template("index.html", result1 = 'Please enter 5 dices in hand', result2='')
+        except:
+            # throw error
+            Exception('Invalid input')
+            return render_template("index.html", result1 = 'Invalid input', result2='')
+
+        my_player = player(dice_in_hand)
+        player_num = int(request.form.get("player_num"))
+        game_dice_num = int(request.form.get("game_dice_num"))
+        game_point_chosen = int(request.form.get("game_point_chosen"))
+        r1, r2 = my_player.technical_analysis(player_num, game_dice_num, game_point_chosen)
+        return(render_template("index.html", result1 = r1, result2=r2, last_input = last_input))
     else:
-        return(render_template("index.html", result1 = "Waiting", result2="Waiting"))
+        return(render_template("index.html", result1 = "Waiting", result2="Waiting", last_input = ''))
 
 if __name__ == "__main__":
     app.run()
